1257138ec08c0db0db169e6cfb38292ea8905e18
naiarakostiureskipereira-boop <naiara.kostiureski.pereira@escola.pr.gov.br>
2026-09-04 10:30:14 -0300

    adds

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avanca = document.querSelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach(button => [ 
+avanca.forEach(button => {
     button.addEventListener('click', function(){
-        const atual = document.querSelectorAll('.ativo');
-        const proximoPasso = 'passo-' +this.getAttribute('data-proximo')
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo'); 
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
-])
+})
